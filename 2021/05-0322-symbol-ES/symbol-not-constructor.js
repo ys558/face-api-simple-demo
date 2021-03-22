2d3148f51f88f8f7ab94212a562734450fe21606
yuyi <38847066+ys558@users.noreply.github.com>
2021-03-22 13:32:28 +0800

    Symbol ES

---
+++ 2021/05-0322-symbol-ES/symbol-not-constructor.js
@@ -0,0 +1,9 @@
+let emperor = {
+  prince: ['prince1', 'prince2', 'prince3'],
+  princess: ['princess1', 'princess2',],
+}
+
+// ! error
+// ! TypeError: Symbol is not a constructor:
+// // const prince1 =  new Symbol('如花')
+// // emperor[prince1] = '如花'
